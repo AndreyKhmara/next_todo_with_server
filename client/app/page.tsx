a027ebf8dfdf79dcda6dee5b30bd38df766caf9f
AndreyKhmara <kelevra101smu@gmail.com>
2024-12-16 22:35:51 +0300

    install_daisyUI_create_components_prettier

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,12 @@
+import {AddTask} from "@/app/components/AddTask";
+
 export default function Home() {
   return (
       //todo stop on 8 minute, need to ammend commit
       <main className="max-w-4xl mx-auto mt-4">
           <div className="text-center my-5 flex flex-col gap-4">
               <h1 className="text-2xl font-bold">Todo List App</h1>
-              hello
+                <AddTask />
           </div>
       </main>
   );
