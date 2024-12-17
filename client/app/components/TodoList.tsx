6f781b1e50a556fe5a15ce1ea62c6bc02519ad6f
AndreyKhmara <kelevra101smu@gmail.com>
2024-12-17 23:00:37 +0300

    add api, add mock data

--- client/app/components/TodoList.tsx
+++ client/app/components/TodoList.tsx
@@ -1,19 +1,28 @@
-export const TodoList = () => {
+import { ITask } from "@/typse/tasks";
+interface ITodoListProps {
+  tasks: ITask[];
+}
+
+//todo stop on 22
+// at first start mock server (npm run json-server)
+export const TodoList: React.FC<ITodoListProps> = ({ tasks }) => {
   return (
     <div>
       <div className="overflow-x-auto">
         <table className="table">
           <thead>
             <tr>
-              <th>Name</th>
-              <th>Favorite Color</th>
+              <th>Tasks</th>
+              <th>Actions</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Cy Ganderton</td>
-              <td>Quality Control Specialist</td>
-            </tr>
+            {tasks.map((tasks) => (
+              <tr key={tasks.id}>
+                <td>{tasks.text}</td>
+                <td>{tasks.text}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
